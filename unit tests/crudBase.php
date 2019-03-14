cb7a94c5508df94599ad155d3ca91dcd15f78950
4ssHole <juanpaologillera@gmail.com>
2019-03-14 15:16:29 +0800

    simplified crudbase

--- unit tests/crudBase.php
+++ unit tests/crudBase.php
@@ -2,7 +2,7 @@
   session_start();
   ob_start();
 
-  include("Connection.php");
+  include("../Connection.php");
 ?>
 <!doctype html>
 <html>
@@ -11,46 +11,34 @@
   <meta charset="utf-8">
   <title>fook uff</title>
   <script src="../jQuery331.js"></script>
-  <link rel="stylesheet" href="../styles.css">
+  <link rel="stylesheet" href="stylesTesting.css">
 </head>
 
 <body>
-
-<?php 
-  $categoryNameArray = $tableHeaderNamesArray = $addArray = array();
-
-  $categories = mysqli_query($connection, "SELECT * FROM `categories`");
-
-  while ($row = mysqli_fetch_array($categories)) { 
-    array_push($categoryNameArray,$row[1]);
-  }
-
-  $tableitems = mysqli_query($connection, "SELECT * FROM items");
-  
-  while ($property = mysqli_fetch_field($tableitems)) {
-    array_push($tableHeaderNamesArray, $property->name);
-    if($property->name!="Date Updated" && $property->name !="Date Added"){
-      array_push($addArray, $property->name);
-    }
-  }
-?>
-
-
 <div class="TableContainer" style="margin:1em;">
   
     <table id="customers">
 
         <?php  
-            if($_SESSION["FilterValue"] != 'All items')  
-            $tableitems = mysqli_query($connection, "SELECT * FROM `! list of all items` WHERE Category ='".$_SESSION["FilterValue"]."';");
-            
-            foreach($tableHeaderNamesArray as $tableheader)  echo '<th>'.$tableheader.'</th>'; //header
+          $tableHeaderNamesArray = array();
 
-            while ($row = mysqli_fetch_array($tableitems)) { //row
-                echo '<tr id="'.$row[0].'">';
-                foreach ($tableHeaderNamesArray as $item) echo '<td><label class="button">'.$row[$item].'</label>';//data
-                echo '</tr>';
-            }
+          $tableitems = mysqli_query($connection, "SELECT * FROM items");
+
+          while ($property = mysqli_fetch_field($tableitems)) {
+            array_push($tableHeaderNamesArray, $property->name);
+          }  
+          
+          foreach($tableHeaderNamesArray as $tableheader)  
+          echo '<th>'.$tableheader.'</th>'; //header
+
+          while ($row = mysqli_fetch_array($tableitems)) { //row
+              echo '<tr id="'.$row[0].'">';
+              
+              foreach ($tableHeaderNamesArray as $item) 
+              echo '<td>'.$row[$item].'</td>';  //data
+
+              echo '</tr>';
+          }
         ?>
     </table>
 </div>
