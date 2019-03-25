d77b21c6ee3151f8bffea646f3bf9839dcebea44
4ssHole <31466851+4ssHole@users.noreply.github.com>
2019-03-25 15:00:41 +0800

    Revert "Revert "idk other files, fixed tr click blocking checkboxes""
    This reverts commit 9a3c37426f8da7a4a9feb7ab22f889bddefd9f10.

--- unit tests/removeItem.php
+++
@@ -1,4 +0,0 @@
-<?php
-    include("../Connection.php");
-    $pdo->query('DELETE FROM items WHERE itemcode IN ('.implode(', ', $_POST['deleteItems']).')');
-?>
--- unit tests/updateItem.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    include("../Connection.php");
-    $pdo->query("UPDATE items SET ".implode(', ', $_POST['updateItems'])." WHERE itemcode='".$_POST['selectedItem']."'");
-?>
